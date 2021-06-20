8072a0de0a41fcf21f8d64752b41dfb1936ffd9d
saranshra16 <82969292+saranshra16@users.noreply.github.com>
2021-06-20 16:21:44 +0530

    changes

--- header.php
+++ header.php
@@ -15,7 +15,11 @@
           <li><a href="lenders.php">Lenders</a></li>
           <li><a href="appraisers.php">Appraisers</a></li>
           <li><a href="contact.php">Contact Us</a></li>
+<<<<<<< Updated upstream
           <li><a href="https://acevaluemanagement.spurams.com/login.aspx" class="btn btn-outline-danger text-danger px-4 ms-md-0 mx-2 mx-md-0 ms-md-4">Login</a></li>
+=======
+          <li><a href="https://acevaluemanagement.spurams.com/login.aspx" class="btn btn-outline-danger text-danger px-4 ms-0 ms-md-4">Login / Sign Up</a></li>
+>>>>>>> Stashed changes
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
