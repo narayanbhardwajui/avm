f51c4ff8cce18040a99c7bbe50b8f6ad8491a381
ui-narayan <ui.narayanbhardwaj@gmail.com>
2021-06-20 20:55:42 +0530

    footer

--- footer.php
+++ footer.php
@@ -1,34 +1,74 @@
   <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="container d-md-flex py-4">
+  <footer id="footer" class="rs-footer home9-style main-home">
+            <div class="footer-top py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
+                            <div class="footer-logo mb-3">
+                                <a href="index.html"><img src="assets/img/logo.svg" width="100px" alt=""></a>
+                            </div>
+                              <div class="textwidget pr-60 md-pr-15">
+                                <p class="white-color mb-2 pe-5">Ace Value Management, as a real estate valuation services firm, founded on the principle of creating transparent relationship between the lender and appraiser through a process that brings out best in both parties.</p>
+                                <a href="about.php" class="text-primary text-decoration-underline">Read More</a>
+                              </div>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
+                            <h4 class="widget-title">Contact</h4>
+                            <ul class="address-widget">
+                                <li class="d-flex mb-3">
+                                <i class="bi bi-geo-alt"></i>
+                                    <div class="desc">695 S Harvard bl 2nd floor <br>Los Angeles, Ca. 90005</div>
+                                </li>
+                                <li class="d-flex mb-3">
+                                  <i class="bi bi-telephone"></i>
+                                    <div class="desc">
+                                       <a href="tel:(213) 338-7887">(213) 338-7887</a>
+                                    </div>
+                                </li>
+                                <li class="d-flex">
+                                <i class="bi bi-envelope"></i>
+                                    <div class="desc">
+                                        <a href="mailto:info@acevaluemanagement.com">info@acevaluemanagement.com</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget footer-links">
+                            <h4 class="widget-title">Links</h4>
+                            <ul class="quick-links">
+                              <li><a class="active" href="index.php">Home</a></li>
+                              <li><a href="services.php">Services</a></li>
+                              <li><a href="about.php">About Us</a></li>
+                              <li><a href="lenders.php">Lenders</a></li>
+                              <li><a href="appraisers.php">Appraisers</a></li>
+                              <li><a href="contact.php">Contact Us</a></li>
+                              <li><a href="https://acevaluemanagement.spurams.com/login.aspx">Login</a></li>
+                          </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget">
+                            <h4 class="widget-title">Social Links</h4>
 
-      <div class="me-md-auto text-center text-md-start">
-        <p><img src="assets/img/logo.svg" alt="" width="100px"></p>
-        <p class="text-light"><strong>Ace Value Management</strong><br>
-              695 S Harvard bl 2nd floor<br>
-                  Los Angeles, Ca. 90005</p>
-      </div>
-      <div>
-      <ul class="list-inline">
-          <li class="list-inline-item"><a class="text-light" href="index.php">Home</a></li>
-          <li class="list-inline-item"><a class="text-light" href="services.php">Services</a></li>
-          <li class="list-inline-item"><a class="text-light" href="about.php">About Us</a></li>
-          <li class="list-inline-item"><a class="text-light" href="contact.php">Contact Us</a></li>
-        </ul>
-      <div class="social-links text-end pt-3 pt-md-0 ">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-      </div>
-      </div>
-    </div>
-    <div class="copyright text-center">
-      <p class="mb-1">&copy; Copyright <strong><span>AVM</span></strong>. All Rights Reserved.</p>
-      <div class="credits">
-          Designed by <a href="https://easygobranding.com/"> EasyGo Branding</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+                            <div class="social-links pt-3 pt-md-0 ">
+                              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom pt-5 border-top" style="border-color:#2c2c2c !important;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <div class="copyright">
+                                <p class="mb-0">© 2021 All Rights Reserved. Developed By <a href="#" class="text-primary">EasyGoBranding</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
