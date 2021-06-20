bcd77725ddd23343971f08f423b4c3ba5cb7c2e0
ui-narayan <ui.narayanbhardwaj@gmail.com>
2021-06-20 15:25:57 +0530

    changes

---
+++ condominium.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Services- AVM</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php include 'header.php';?>
+
+  <main id="main">
+    <div class="text-center"
+      style="background: url(https://images.pexels.com/photos/3184424/pexels-photo-3184424.jpeg) no-repeat top / 100%;height: 45vh;">
+    </div>
+    <section id="about-us" class="about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-header">
+                Services
+              </div>
+              <div class="card-body">
+                <ul class="ps-3" style="font-size: 14px;">
+                  <li><a href="?page_id=9">1004 – Single Family</a></li>
+                  <li><a href="?page_id=97">1073 – Condominium</a></li>
+                  <li><a href="?page_id=193">2090 – COOP</a></li>
+                  <li><a href="?page_id=282">1025 – Small Residential Income Property Appraisal</a></li>
+                  <li><a href="?page_id=217">2055 – Exterior Single Family</a></li>
+                  <li><a href="?page_id=227">1075 – Exterior Condominium</a></li>
+                  <li><a href="?page_id=232">2095 – Exterior COOP</a></li>
+                  <li><a href="?page_id=14">1004C – Manufacture Housing</a></li>
+                  <li><a href="?page_id=236">2000 – Field/Desk Review</a></li>
+                  <li><a href="?page_id=278">2006 – Residential Appraisal Desk and/or Field Review Report</a></li>
+                  <li><a href="?page_id=240">1004D – Update of Value</a></li>
+                  <li><a href="?page_id=240">216, 1007 Additional Forms</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-9 ">
+            <div class="entry-content card-body bg-light border">
+              <h2 class="fw-bold text-primary">Services</h2>
+              <p>Lender’s Link has developed a line of off-the-shelf products that satisfies the requirements of even
+                our most demanding clients. &nbsp;By tackling the most common appraisal scenarios and service
+                requirements in a quick and practical way, our products effectively satiate their ever-growing needs:
+              </p>
+              <div class="mb-5 border-bottom mt-5">
+                <h3>RESIDENTIAL PROPERTIES</h3>
+                <p><a href="?page_id=9">1004 – Uniform Residential Appraisal Report</a></p>
+                <ul>
+                  <li>Fannie Mae 1004 | Freddie Mac 70</li>
+                </ul>
+                <p><a href="?page_id=97">1073 – Individual Condominium Unit Appraisal Report</a></p>
+                <ul>
+                  <li>Fannie Mae 1073 | Freddie Mac 465</li>
+                </ul>
+                <p><a href="?page_id=193">2090 – Individual Cooperative Interest Appraisal Report</a></p>
+                <ul>
+                  <li>Fannie Mae 2090</li>
+                </ul>
+              </div>
+              <div class="mb-5 border-bottom mt-5">
+                <h3>multi-family properties</h3>
+                <p><a href="?page_id=282">1025 – Small Residential Income Property Appraisal</a></p>
+              </div>
+              <div class="mb-5 border-bottom mt-5">
+                <h3>EXTERIOR ONLY APPRAISAL REPORTS</h3>
+                <p><a href="?page_id=217">2055 – Exterior-Only Inspection Residential Appraisal Report</a></p>
+                <ul>
+                  <li>Fannie Mae 2055 | Freddie Mac 2055</li>
+                </ul>
+                <p><a href="?page_id=227">1075 – Exterior-Only Inspection Individual Condominium Unit Appraisal
+                    Report</a>
+                </p>
+                <ul>
+                  <li>Fannie Mae 1075 | Freddie Mac 466</li>
+                </ul>
+                <p><a href="?page_id=232">2095 – Exterior-Only Inspection Individual Cooperative Interest Appraisal
+                    Report</a></p>
+                <ul>
+                  <li>Fannie Mae 2095</li>
+                </ul>
+              </div>
+              <div class="mb-5 border-bottom mt-5">
+                <h3>MANUFACTURED PROPERTIES</h3>
+                <p><a href="?page_id=14">1004C – Manufactured Home Appraisal Report</a></p>
+                <ul>
+                  <li>Fannie Mae 1004C | Freddie Mac 465</li>
+                </ul>
+                <h3>EXTERIOR/INTERIOR FIELD or DESK REVIEW APPRAISAL REPORTS</h3>
+                <p><a href="?page_id=236">2000 – One-Unit Residential Appraisal Field Review Report</a></p>
+                <ul>
+                  <li>Fannie Mae 2000 | Freddie Mac 1032</li>
+                </ul>
+                <p><a href="?page_id=278">2006 – One-Unit Residential Appraisal Desk Review Report</a></p>
+                <ul>
+                  <li>Fannie Mae 2006 | Freddie Mac</li>
+                </ul>
+              </div>
+              <div class="mb-5 border-bottom mt-5">
+                <h3>ADDITIONAL FORMS AND ADDEDUMS</h3>
+                <p><a href="?page_id=240">1007 – Single-Family Comparable Rent Schedule</a></p>
+                <ul>
+                  <li>Fannie Mae 1007 | Freddie Mac 1000</li>
+                </ul>
+                <p><a href="?page_id=240">1004D – Appraisal Update and/or Completion Report (1004D)</a></p>
+                <ul>
+                  <li>Fannie Mae 1004D | Freddie Mac 442</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+  </main><!-- End #main -->
+
+  <?php include 'footer.php';?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
